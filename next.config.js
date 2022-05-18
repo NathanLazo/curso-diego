99aecaef68bbefb94850f395d974f94d5cadc359
Nathan Lazo <nathanlazo@outlook.com>
2022-05-17 21:53:32 -0600

    [CONFIG] Succesful config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    // This is the default value, but we set it explicitly to avoid
+  },
+  compress: true,
 }
 
 module.exports = nextConfig
